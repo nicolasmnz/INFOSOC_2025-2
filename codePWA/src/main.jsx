2f0071f716c7bf78c98c9ba0ea3cd0b2b15c99a3
doxter6 <163431585+doxter6@users.noreply.github.com>
2025-05-11 17:13:19 -0400

    Adaptacion de la aplicacion

--- codePWA/src/main.jsx
+++ codePWA/src/main.jsx
@@ -1,17 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-
+import './index.css'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode> {//habilitamos modo stricto para ver errores en el desarrollo
-    } 
-    <BrowserRouter> {// nos da la posibilidad de crear subpaginas
-    }
-      <App />
-    </BrowserRouter>,
-  </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
 
