abae9e0d79e4ba464fcccdea1ae0e9e37f810848
nicolasmnz <nicolas.munozra@usm.cl>
2025-05-11 16:03:39 -0400

    preparacion pwa

--- codePWA/src/main.jsx
+++ codePWA/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './index.css';
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,10 +10,3 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-return (
-  <div className="min-h-screen bg-blue-50 flex items-center justify-center">
-    <h1 className="text-3xl font-bold text-blue-700">
-      ¡Tailwind está funcionando!
-    </h1>
-  </div>
-);
--- codePWA/vite.config.js
+++ codePWA/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
       manifest: {
         name: 'sansAvisos',
         short_name: 'sansApp',
