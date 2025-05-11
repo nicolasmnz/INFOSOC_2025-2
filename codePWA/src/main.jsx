c96380ebf678863235230aa5d39e0c701f9841e8
nicolasmnz <nicolas.munozra@usm.cl>
2025-05-11 16:38:36 -0400

    comienzo desarrollo pwa

--- codePWA/src/main.jsx
+++ codePWA/src/main.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <StrictMode> {//habilitamos modo stricto para ver errores en el desarrollo
+    } 
+    <BrowserRouter> {// nos da la posibilidad de crear subpaginas
+    }
+      <App />
+    </BrowserRouter>,
+  </StrictMode>
 )
 
