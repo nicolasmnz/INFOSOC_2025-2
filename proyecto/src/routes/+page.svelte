6d00dd26465f92f506e1aac4d218fcb82deb2cca
nicolasmnz <nicolas.munozra@usm.cl>
2025-05-22 15:01:47 -0400

    Añadido component iconGrid Es posible hacer href o comunicarse con el backend con metdo POST

--- proyecto/src/routes/+page.svelte
+++ proyecto/src/routes/+page.svelte
@@ -1,70 +1,25 @@
 <script>
-	import Header from '$lib/header.svelte';
+	import Header from '$lib/Header.svelte';
+	import IconsGrid from '$lib/IconsGrid.svelte';
 	let title = '¿Qué problema presentas?';
 
-	import { BookOpenText, Utensils, Toilet, Presentation, GraduationCap } from '@lucide/svelte';
+	import {
+		BookOpenText,
+		Utensils,
+		Toilet,
+		Presentation,
+		GraduationCap,
+		Wifi
+	} from '@lucide/svelte';
 	const items = [
-		{ icon: Utensils, label: 'Casino', color: '#e74c3c' },
-		{ icon: Toilet, label: 'Baños', color: '#3498db' },
-		{ icon: BookOpenText, label: 'Biblioteca', color: '#af50e5' },
-		{ icon: Presentation, label: 'Salas de clases', color: '#ff9f00' },
-		{ icon: GraduationCap, label: 'Salas de estudio', color: '#c27ba0' }
+		{ icon: Utensils, label: 'Casino', color: '#e74c3c', href: '/casino' },
+		{ icon: Toilet, label: 'Baños', color: '#5a5aff', href: '/banos' },
+		{ icon: BookOpenText, label: 'Biblioteca', color: '#af50e5', href: '/biblioteca' },
+		{ icon: Presentation, label: 'Salas de clases', color: '#ff9f00', href: '/salas-clases' },
+		{ icon: GraduationCap, label: 'Salas de estudio', color: '#c27ba0', href: '/salas-estudio' },
+		{ icon: Wifi, label: 'Conectividad', color: '#3498db', href: '/conectividad' }
 	];
 </script>
 
 <Header {title} />
-
-<main class="grid-container">
-	{#each items as { icon: Icon, label, color }}
-		<div class="item">
-			<button class="icon-btn" style="--bg: {color}">
-				<Icon size="2.5rem" color="white" />
-			</button>
-			<div class="icon-label">{label}</div>
-		</div>
-	{/each}
-</main>
-
-<style>
-	.grid-container {
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
-		gap: 1.5rem;
-		padding: 0 1rem;
-		justify-items: center;
-		justify-content: center;
-	}
-	.item {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	.icon-btn {
-		background-color: var(--bg); /* variable que viene del style */
-		border: none;
-		border-radius: 50%; /* esto hace que sea un círculo */
-		padding: 0.75rem;
-		width: 5rem;
-		height: 5rem;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		cursor: pointer;
-		transition: transform 0.1s;
-	}
-	.icon-btn:active {
-		transform: scale(0.95); /* da un efecto de “presionado” */
-	}
-	.icon-label {
-		margin-top: 0.5rem;
-		font-size: 0.9rem;
-		text-align: center;
-		color: #333;
-	}
-	main {
-		margin-top: 7rem;
-		padding-inline: 0.5rem;
-		min-height: calc(100vh - 4em -1.5rem);
-		background: #f9f9f9;
-	}
-</style>
+<IconsGrid {items} size="2.5em" />
---
+++ proyecto/src/routes/banos/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Header from '$lib/Header.svelte';
+	import IconsGrid from '$lib/IconsGrid.svelte';
+
+	import { TriangleAlert, SoapDispenserDroplet, Ban, Droplet, Scroll } from '@lucide/svelte';
+
+	let items = [
+		{ icon: SoapDispenserDroplet, label: 'Sin jabón', color: '#f39c12' },
+		{ icon: Scroll, label: 'Sin papel higiénico', color: '#e74c3c' },
+		{ icon: TriangleAlert, label: 'Baño obstruido', color: '#c0392b' },
+		{ icon: Ban, label: 'Fuera de servicio', color: '#c0392b' },
+		{ icon: Droplet, label: 'Fuga de agua', color: '#5dade2' }
+	];
+	let title = 'Reportar problema en los baños';
+</script>
+
+<Header {title} />
+<IconsGrid {items} size="2.5em" />
